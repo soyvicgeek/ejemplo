cee80702c88fad0003f5e11702c79d5986bc92d8
Daniel Flores <vflorestrejo@gmail.com>
2025-05-14 15:57:05 -0600

    Changed name VDFT01

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,12 @@
 
 export default function Home() {
   return (
-    <h1>Hola Mundo</h1>
+    <div className="bg-red-100 h-screen">
+      <h1 className="text-cyan-500 text-7xl text-center">Hola Mundo</h1>
+
+      <div className="flex flex-col justify-center items-center">
+        <p className="pt-10 text-3xl">Víctor Daniel Flores Trejo</p>
+      </div>
+    </div>
   );
 }
